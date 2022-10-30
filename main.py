3a910dc4dc6440eabd7401677ef886742d4433cb
Steelix209 <113288674+Steelix209@users.noreply.github.com>
2022-10-30 15:01:52 +0200

    add
    add

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from pygame import *
 mixer.init()
 font.init()
-WIDTH = 700
+WIDTH = 900
 HEIGHT = 500
 window = display.set_mode((WIDTH,HEIGHT))
 display.set_caption("Angry Birds")
@@ -19,16 +19,28 @@ class GameSprite(sprite.Sprite):
         self.height = height
     def draw(self):
         window.blit(self.image, self.rect)
+    
 
 class Player(GameSprite):
     def __init__(self,image_name,x,y,width,height,speed,dmg):
         super().__init__(image_name,x,y,width,height)
+    def update(self):
+        click = mouse.get_pressed()
         
+        if click[2]:
+            x,y = mouse.get_pos()
+            if self.rect.collidepoint(x,y) and self.rect.x != 170 and self.rect.y != 380:
+                self.rect.x = 192
+                self.rect.y = 395
 
-        
+
+            
+
+
 class Bird(Player):
     def __init__(self):
         super().__init__("bird.png",100,452,45,45,10,20)
+    
 
 
 class Bird1(Player):
@@ -57,7 +69,10 @@ class Pig(Enemy):
 class Wall(GameSprite):
     def __init__(self,image_name,x,y,width,height):
          super().__init__(image_name,x,y,width,height)
-         
+class Bow(GameSprite):
+    def __init__(self):
+         super().__init__("bow.png",170,400,70,90)
+
 
 walls = sprite.Group()
 pigs = sprite.Group()
@@ -75,6 +90,7 @@ def level1(bg_img):
     wood4 = Wall("wood.png",675 ,290,15,100)
     wood5 = Wall("wood2.png",575 ,290,100,15)
     wood6 = Wall("wood.png",560 ,290,15,100)
+
     pigl1 = Pig("pig.png",605,445,45,45)
     pigl2 = Pig("pig.png",603,345,45,45)
     bird = Bird()
@@ -87,40 +103,40 @@ def level1(bg_img):
 
 def level2(bg_img):
     global bg_image
-    beam1 = Wall("wood.png",640 ,440,10,50)
-    beam2 = Wall("wood.png",590 ,440,10,50)
-    beam3 = Wall("wood2.png",590 ,430,60,10)
+    beam1 = Wall("wood.png",840 ,440,10,50)
+    beam2 = Wall("wood.png",790 ,440,10,50)
+    beam3 = Wall("wood2.png",790 ,430,60,10)
 
-    beam4 = Wall("wood2.png",530 ,430,60,10)
-    beam5 = Wall("wood.png",530 ,440,10,53)
+    beam4 = Wall("wood2.png",730 ,430,60,10)
+    beam5 = Wall("wood.png",730 ,440,10,53)
 
-    beam6 = Wall("wood2.png",480 ,430,50,10)
-    beam7 = Wall("wood.png",480 ,440,10,53)
+    beam6 = Wall("wood2.png",680 ,430,50,10)
+    beam7 = Wall("wood.png",680 ,440,10,53)
 
-    beam8 = Wall("wood.png",615 ,380,10,50)
-    beam9 = Wall("wood2.png",565 ,370,60,10)
-    beam10 = Wall("wood.png",560 ,380,10,50)
+    beam8 = Wall("wood.png",815 ,380,10,50)
+    beam9 = Wall("wood2.png",765 ,370,60,10)
+    beam10 = Wall("wood.png",760 ,380,10,50)
 
-    beam11 = Wall("wood2.png",505 ,370,60,10)
-    beam12 = Wall("wood.png",505 ,380,10,50)
+    beam11 = Wall("wood2.png",705 ,370,60,10)
+    beam12 = Wall("wood.png",705 ,380,10,50)
 
-    beam13 = Wall("wood2.png",530,310,60,10)
-    beam14 = Wall("wood.png",530 ,320,10,50)
-    beam15 = Wall("wood.png",580 ,320,10,50)
+    beam13 = Wall("wood2.png",730,310,60,10)
+    beam14 = Wall("wood.png",730 ,320,10,50)
+    beam15 = Wall("wood.png",780 ,320,10,50)
     walls.add(beam1,beam2,beam3,beam4,beam5,beam6,beam7,beam8,beam9,beam10,beam11,beam12,beam13,beam14,beam15)
-    pig1 = Pig("pig.png",605,463,30,30)
-    pig2 = Pig("pig.png",550,463,30,30)
-    pig3 = Pig("pig.png",495,463,30,30)
-    pig4 = Pig("pig.png",580,400,30,30)
-    pig5 = Pig("pig.png",520,400,30,30)
-    pig6 = Pig("pig.png",545,340,30,30)
+    pig1 = Pig("pig.png",805,463,30,30)
+    pig2 = Pig("pig.png",750,463,30,30)
+    pig3 = Pig("pig.png",695,463,30,30)
+    pig4 = Pig("pig.png",780,400,30,30)
+    pig5 = Pig("pig.png",720,400,30,30)
+    pig6 = Pig("pig.png",745,340,30,30)
     pigs.add(pig1,pig2,pig3,pig4,pig5,pig6)
     bird = Bird()
     bird1 = Bird1()
     bird2 = Bird2()
     birds.add(bird1,bird2,bird)
 
-
+bow = Bow()
 run = True
 level2("background2.png")
 while run:
@@ -129,6 +145,8 @@ while run:
             run = False
 
     window.blit(bg_image,(0,0))
+    birds.update()
+    bow.draw()
     walls.draw(window)
     pigs.draw(window)
     birds.draw(window)
